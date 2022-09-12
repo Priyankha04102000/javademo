fc3ef1bf4a8703a67a919a71a7d1c00c85cffd9b
shanvikha <you@priyankhamohan60@gmail.com>
2022-09-12 16:45:04 +0530

    'assis'

--- Phase-3-assisted-springboot-main/S03springbootmvc/src/main/java/com/example/demo/exceptions/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-	public ProductNotFoundException(int id) {
-		super("Product with id " + id + " not found.");
-	}
-
-}
--- Phase-3-assisted-springboot-main/ecommwebapp/src/main/java/com/example/ecommwebapp/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommwebapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ecommwebapp.entity.ShoppingCart;
-
-public interface ShoppingCartRepository extends CrudRepository<ShoppingCart, Integer>{
-
-}
